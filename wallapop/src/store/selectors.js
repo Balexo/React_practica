afc344fb468397f745eef8d94fe0a07eb08b838f
Oscar Vallejo <oscar.vallejo@outlook.com>
2024-06-25 14:48:02 +0200

    Finally works

--- wallapop/src/store/selectors.js
+++ wallapop/src/store/selectors.js
@@ -1,13 +1,9 @@
 export const getIsLogged = (state) => state.auth;
-export const getListAds = (state) => state.ads.data;
-console.log(getListAds());
+export const getListAds = (state) => state.ads;
 
 export const getAd = (adId) => (state) => {
-  //const adsArray = Object.values(state.ads.data);
   const ad = state.ads.find((ad) => ad.id === adId);
   console.log(state);
-  //console.log(adsArray);
-  //const ad = adsArray.find((ad) => ad.id === adId);
   console.log(ad);
   return ad;
 };
